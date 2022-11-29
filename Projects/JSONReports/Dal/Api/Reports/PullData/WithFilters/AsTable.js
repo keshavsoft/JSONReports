75d34991f4e5642c29905216dbb2327c6b38f24e
keshavsoft <nknnkumar@yahoo.com>
2022-11-29 18:42:01 +0530

    keshav

--- Projects/JSONReports/Dal/Api/Reports/PullData/WithFilters/AsTable.js
+++ Projects/JSONReports/Dal/Api/Reports/PullData/WithFilters/AsTable.js
@@ -1,11 +1,19 @@
-let DataSupplySendDataWithConfig = require("../../../../../../../DataSupply/Reports/Ledger/WithFilters");
+//let DataSupplySendDataWithConfig = require("../../../../../../../DataSupply/Reports/Ledger/WithFilters");
+let DataSupplyToDal = require("../../../../../../../DataSupply/Reports/ToDal/WithFilters/StartFuncs");
 
 let DataWithConfig = async ({ inReportName, inDataPK, inFilterObject }) => {
     if (inDataPK > 0) {
-        return await DataSupplySendDataWithConfig.StartFunc({
-            inLedgerName: inReportName,
-            inDataPK, inFilterObject
+        //console.log("inReportName, inDataPK, inFilterObject  : ", inReportName, inDataPK, inFilterObject);
+        // return await DataSupplySendDataWithConfig.StartFunc({
+        //     inLedgerName: inReportName,
+        //     inDataPK, inFilterObject
+        // });
+        return await DataSupplyToDal.StartFunc({
+            inLedgerName: inReportName, inDataPK,
+            inFilterObject
         })
+
+        //return await {};
     };
 };
 
--- Projects/JSONReports/Routes/Api/Reports/PullData/WithFilters/AsTable.js
+++ Projects/JSONReports/Routes/Api/Reports/PullData/WithFilters/AsTable.js
@@ -2,10 +2,35 @@ let express = require("express");
 let Router = express.Router();
 let Repo = require("../../../../../Repository/Api/Reports/PullData/WithFilters/AsTable")
 
-Router.post('/DataWithConfig/:inReportName', (req, res) => {
+Router.post('/', (req, res) => {
+    let LocalDataPK;
+
+    let LocalFilterObject = req.body;
+    let LocalReportName = LocalFilterObject.ReportName;
+    delete LocalFilterObject.ReportName;
+    console.log("LocalFilterObject : ", LocalFilterObject);
+    if ("KeshavSoft" in req) {
+        if ("DataPk" in req.KeshavSoft) {
+            LocalDataPK = req.KeshavSoft.DataPk;
+
+            Repo.DataWithConfig({
+                inReportName: LocalReportName,
+                inDataPK: LocalDataPK,
+                inFilterObject: LocalFilterObject
+            }).then(PromiseData => {
+                res.end(JSON.stringify(PromiseData));
+            }).catch();
+        } else {
+            res.json({ KTF: false, KReason: "DataPk not found in Request.Keshavsoft" });
+        };
+    } else {
+        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" });
+    };
+});
+
+Router.get('/DataWithConfigFromWithOutFilters/:inReportName', (req, res) => {
     let LocalDataPK;
     let LocalReportName;
-    let LocalFilterObject = req.body;
 
     if ("KeshavSoft" in req) {
         if ("DataPk" in req.KeshavSoft) {
@@ -17,7 +42,6 @@ Router.post('/DataWithConfig/:inReportName', (req, res) => {
                 Repo.DataWithConfig({
                     inReportName: LocalReportName,
                     inDataPK: LocalDataPK,
-                    inFilterObject: LocalFilterObject
                 }).then(PromiseData => {
                     res.end(JSON.stringify(PromiseData));
                 }).catch();
@@ -32,5 +56,6 @@ Router.post('/DataWithConfig/:inReportName', (req, res) => {
         res.json({ KTF: false, KReason: "KeshavSoft not found in Request" });
     };
 });
+
 
 module.exports = Router;
